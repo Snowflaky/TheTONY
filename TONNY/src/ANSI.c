663805523f930e23be1a11de2fcab6a1e9155445
Sara <s194131@student.dtu.dk>
2021-01-20 17:41:57 +0100

    dssav

--- TONNY/src/ANSI.c
+++ TONNY/src/ANSI.c
@@ -864,75 +864,47 @@ uint16_t updateHighscore (uint16_t highscore, uint16_t score){
 //Sets LED diode to a specific color
 void setLed(uint8_t color) {
 //0=reset, 1=red(PB4), 2=green(PC7), 3=blue(PA9), 4=yellow(PB4+PC7)
+    GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2));
+    GPIOB->OSPEEDR |= (0x00000002 << (4 * 2));
+    GPIOB->OTYPER &= ~(0x0001 << (4));
+    GPIOB->OTYPER |= (0x0000 << (4));
+    GPIOB->MODER &= ~(0x00000003 << (4 * 2));
+    GPIOB->MODER |= (0x00000001 << (4 * 2));
+
+    GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2));
+    GPIOC->OSPEEDR |= (0x00000002 << (7 * 2));
+    GPIOC->OTYPER &= ~(0x0001 << (7));
+    GPIOC->OTYPER |= (0x0000 << (7));
+    GPIOC->MODER &= ~(0x00000003 << (7 * 2));
+    GPIOC->MODER |= (0x00000001 << (7 * 2));
+
+    GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2));
+    GPIOA->OSPEEDR |= (0x00000002 << (9 * 2));
+    GPIOA->OTYPER &= ~(0x0001 << (9));
+    GPIOA->OTYPER |= (0x0000 << (9));
+    GPIOA->MODER &= ~(0x00000003 << (9 * 2));
+    GPIOA->MODER |= (0x00000001 << (9 * 2));
+
     if (color==0){
             //resets red LED
-        GPIOB->OSPEEDR |= (0x00000003 << (4 * 2));
-        GPIOB->OSPEEDR &= ~(0x00000002 << (4 * 2));
-        GPIOB->OTYPER |= (0x0001 << (4));
-        GPIOB->OTYPER &= ~(0x0000 << (4));
-        GPIOB->MODER |= (0x00000003 << (4 * 2));
-        GPIOB->MODER &= ~(0x00000001 << (4 * 2));
-        GPIOB->ODR &= ~(0x0000 << 4);
+        GPIOB->ODR |= (0x0001 << 4);
             //resets green LED
-        GPIOC->OSPEEDR |= (0x00000003 << (7 * 2));
-        GPIOC->OSPEEDR &= ~(0x00000002 << (7 * 2));
-        GPIOC->OTYPER |= (0x0001 << (7));
-        GPIOC->OTYPER &= ~(0x0000 << (7));
-        GPIOC->MODER |= (0x00000003 << (7 * 2));
-        GPIOC->MODER &= ~(0x00000001 << (7 * 2));
-        GPIOC->ODR &= ~(0x0000 << 7);
+        GPIOC->ODR |= (0x0001 << 7);
             //resets blue LED
-        GPIOA->OSPEEDR |= (0x00000003 << (9 * 2));
-        GPIOA->OSPEEDR &= ~(0x00000002 << (9 * 2));
-        GPIOA->OTYPER |= (0x0001 << (9));
-        GPIOA->OTYPER &= ~(0x0000 << (9));
-        GPIOA->MODER |= (0x00000003 << (9 * 2));
-        GPIOA->MODER &= ~(0x00000001 << (9 * 2));
-        GPIOA->ODR &= ~(0x0000 << 9);
+        GPIOA->ODR |= (0x0001 << 9);
     }
     if (color==1) {//red
-        GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2));
-        GPIOB->OSPEEDR |= (0x00000002 << (4 * 2));
-        GPIOB->OTYPER &= ~(0x0001 << (4));
-        GPIOB->OTYPER |= (0x0000 << (4));
-        GPIOB->MODER &= ~(0x00000003 << (4 * 2));
-        GPIOB->MODER |= (0x00000001 << (4 * 2));
         GPIOB->ODR |= (0x0000 << 4); //set pin to low
     }
     if (color==2) {//green
-        GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2));
-        GPIOC->OSPEEDR |= (0x00000002 << (7 * 2));
-        GPIOC->OTYPER &= ~(0x0001 << (7));
-        GPIOC->OTYPER |= (0x0000 << (7));
-        GPIOC->MODER &= ~(0x00000003 << (7 * 2));
-        GPIOC->MODER |= (0x00000001 << (7 * 2));
         GPIOC->ODR |= (0x0000 << 7); //set pin to low
     }
     if (color==3) {//blue
-        GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2));
-        GPIOA->OSPEEDR |= (0x00000002 << (9 * 2));
-        GPIOA->OTYPER &= ~(0x0001 << (9));
-        GPIOA->OTYPER |= (0x0000 << (9));
-        GPIOA->MODER &= ~(0x00000003 << (9 * 2));
-        GPIOA->MODER |= (0x00000001 << (9 * 2));
         GPIOA->ODR |= (0x0000 << 9); //set pin to low
 
     }
     if (color==4){//yellow
-        GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2));
-        GPIOB->OSPEEDR |= (0x00000002 << (4 * 2));
-        GPIOB->OTYPER &= ~(0x0001 << (4));
-        GPIOB->OTYPER |= (0x0000 << (4));
-        GPIOB->MODER &= ~(0x00000003 << (4 * 2));
-        GPIOB->MODER |= (0x00000001 << (4 * 2));
         GPIOB->ODR |= (0x0000 << 4);
-
-        GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2));
-        GPIOC->OSPEEDR |= (0x00000002 << (7 * 2));
-        GPIOC->OTYPER &= ~(0x0001 << (7));
-        GPIOC->OTYPER |= (0x0000 << (7));
-        GPIOC->MODER &= ~(0x00000003 << (7 * 2));
-        GPIOC->MODER |= (0x00000001 << (7 * 2));
         GPIOC->ODR |= (0x0000 << 7);
     }
 }
